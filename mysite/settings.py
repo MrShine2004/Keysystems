05af72a10203465049e945c2a9b437d919aff758
Alex <alex.yamolkin@gmail.com>
2024-07-22 22:10:11 +0300

    CORS2

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,8 +66,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
+    "http://localhost:8080/",
+    "http://127.0.0.1:8080/",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
