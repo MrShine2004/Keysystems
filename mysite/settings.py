e10f23d50cddc29e60ce9fec30167d48f1830abb
Alex <alex.yamolkin@gmail.com>
2024-07-23 12:57:22 +0300

    Repair

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
                       'content-type', 'accept', 'origin', 'Authorization',
